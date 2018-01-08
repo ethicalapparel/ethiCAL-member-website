8a4b0c5123ff5e8771a3b164fb4714482900d251
Andrew Linxie <alinxie97@gmail.com>
2018-01-07 19:36:45 -0800

    Restruct Website Components

---
+++ client/src/components/Calendar/index.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import './index.css';
+
+class Calendar extends Component {
+  state = {
+      data: []
+  };
+
+  componentDidMount() {
+    axios.get('/asana/calendar')
+      .then((response) => this.setState({data: response.data}));
+  };
+
+  render() {
+    return (
+      <div>
+        <h1> Club Calendar </h1>
+        <h1> {JSON.stringify(this.state.data)} </h1>
+      </div>
+    );
+  };
+};
+
+export default Calendar;
---
+++ client/src/components/Feedback/index.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Form, Grid, Header } from 'semantic-ui-react';
+import './index.css';
+
+class Feedback extends Component {
+  state = {
+    feedbackSubmitted: false,
+    feedback: '',
+    description: ''
+  };
+
+
+  submit = () => {
+    // API Call to submit feedback
+    axios.post('/asana/submitFeedback',
+      {name: this.state.feedback, notes: this.state.description});
+    this.setState({feedbackSubmitted: true, feedback: '', description: ''});
+  };
+
+
+  handleChange = (e, { name, value }) => this.setState({ [name]: value });
+
+  render() {
+    return (
+      <Grid centered columns={1} style={{marginTop: '20vh'}}>
+        <Form onSubmit={this.submit}>
+          <Header as='h1'> Give Feedback </Header>
+          <label> pls do not spam </label>
+          <Form.Field>
+            <Form.Input placeholder='Feedback' name='feedback' value={this.state.feedback} onChange={this.handleChange}/>
+          </Form.Field>
+
+          <Form.Field>
+            <Form.TextArea placeholder='Description' name='description' value={this.state.description} onChange={this.handleChange}/>
+          </Form.Field>
+
+          <Form.Button content='Submit'/>
+          <div>
+          {this.state.feedbackSubmitted && "Thank you for your submission!"}
+          </div>
+        </Form>
+      </Grid>
+    );
+  };
+};
+
+export default Feedback;
