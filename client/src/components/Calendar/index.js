a86cdc84bcec21bd99016a8654e279574bfec9aa
Andrew Linxie <alinxie97@gmail.com>
2018-01-18 00:43:42 -0800

    Added packages

--- client/src/components/Calendar/index.js
+++ client/src/components/Calendar/index.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import BigCalendar from 'react-big-calendar';
+import moment from 'moment';
 import './index.css';
+
+BigCalendar.momentLocalizer(moment);
 
 class Calendar extends Component {
   state = {
@@ -17,6 +21,7 @@ class Calendar extends Component {
       <div>
         <h1> Club Calendar </h1>
         <h1> {JSON.stringify(this.state.data)} </h1>
+        <BigCalendar step={60} defaultDate={new Date(2015,3,1)}> </BigCalendar>
       </div>
     );
   };
